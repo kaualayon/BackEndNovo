4df348fc22e29b770dedf5012ac298c525340092
kaualayon <limakaua582@gmail.com>
2024-10-29 15:21:02 -0300

    atualizado

--- Back-End/biblioteca-frontend/src/App.vue
+++ Back-End/biblioteca-frontend/src/App.vue
@@ -1,42 +1,8 @@
 <template>
   <div id="app">
-    <header>
-      <nav class="navbar">
-        <ul class="nav-right">
-          <li><router-link to="/home">Início</router-link></li>
-          <li><router-link to="/produtos">Produtos</router-link></li>
-          <li><router-link to="/sobre">Sobre</router-link></li>
-          <li><router-link to="/login">Entrar</router-link></li>
-          <li><router-link to="/register">Criar Conta</router-link></li>
-        </ul>
-      </nav>
-    </header>
-
     <main>
       <router-view></router-view> <!-- Renderiza o componente correspondente à rota -->
     </main>
-
-    <footer class="footer">
-      <div class="footer-content">
-        <div class="footer-left">
-          <h4>MANGE BOOK</h4>
-          <p>© 2024 Todos os direitos reservados.</p>
-        </div>
-        <div class="footer-middle">
-          <ul>
-            <li><router-link to="/home">Início</router-link></li>
-            <li><router-link to="/produtos">Produtos</router-link></li>
-            <li><router-link to="/sobre">Sobre</router-link></li>
-          </ul>
-        </div>
-        <div class="footer-right">
-          <p>Siga-nos:</p>
-          <a href="#"><i class="fa fa-facebook"></i></a>
-          <a href="#"><i class="fa fa-twitter"></i></a>
-          <a href="#"><i class="fa fa-instagram"></i></a>
-        </div>
-      </div>
-    </footer>
   </div>
 </template>
 
@@ -45,4 +11,5 @@ export default {
   name: 'App',
 };
 </script>
+
 
--- Back-End/biblioteca-frontend/src/main.js
+++ Back-End/biblioteca-frontend/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue';
 import router from './router'; // Certifique-se de que o caminho esteja correto
 import './assets/css/style.css';
 
+
 createApp(App)
   .use(router) // Aqui é onde você deve usar o router
   .mount('#app');
