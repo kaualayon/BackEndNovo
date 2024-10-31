bf5abf118f01deaa7370835781e1a1e6b9436736
kaualayon <limakaua582@gmail.com>
2024-10-31 02:57:59 -0300

    atualizado

--- Back-End/biblioteca-frontend/src/views/BookDetail.vue
+++ Back-End/biblioteca-frontend/src/views/BookDetail.vue
@@ -129,6 +129,116 @@
           reviewCount: 320,
           image: '../images/fahrenheit_451.jpg', // Imagem de Fahrenheit 451
         },
+
+        {
+          id: 10,
+          title: 'A Menina que Roubava Livros',
+          author: 'Markus Zusak',
+          price: 45.00,
+          rating: 4.8,
+          reviewCount: 400,
+          image: '../images/a_menina_que_roubava_livros.jpg', // Imagem de A Menina que Roubava Livros
+        },
+
+        {
+          id: 11,
+          title: 'O Sol é para Todos',
+          author: 'Harper Lee',
+          price: 42.00,
+          rating: 4.9,
+          reviewCount: 450,
+          image: '../images/o_sol_e_para_todos.jpg', // Imagem do livro "O Sol é para Todos"
+        },
+
+        {
+          id: 12,
+          title: 'Orgulho e Preconceito',
+          author: 'Jane Austen',
+          price: 35.00,
+          rating: 4.8,
+          reviewCount: 600,
+          image: '../images/orgulho_e_preconceito.jpg', // Imagem do livro "Orgulho e Preconceito"
+        },
+
+        {
+          id: 13,
+          title: 'O Hobbit',
+          author: 'J.R.R. Tolkien',
+          price: 45.00,
+          rating: 4.9,
+          reviewCount: 750,
+          image: '../images/o_hobbit.jpg', // Imagem do livro "O Hobbit"
+        },
+
+        {
+          id: 14,
+          title: 'O Conto da Aia',
+          author: 'Margaret Atwood',
+          price: 59.90,
+          rating: 4.6,
+          reviewCount: 500,
+          image: '../images/o_conto_da_aia.jpg', // Imagem do livro "O Conto da Aia"
+        },
+
+        {
+          id: 15,
+          title: 'A Sombra do Vento',
+          author: 'Carlos Ruiz Zafón',
+          price: 49.90,
+          rating: 4.7,
+          reviewCount: 750,
+          image: '../images/a_sombra_do_vento.jpg', // Imagem do livro "A Sombra do Vento"
+        },
+
+        {
+          id: 16,
+          title: 'Os Miseráveis',
+          author: 'Victor Hugo',
+          price: 59.90,
+          rating: 4.9,
+          reviewCount: 1250,
+          image: '../images/os_miseraveis.jpg', // Imagem do livro "Os Miseráveis"
+        },
+
+        {
+          id: 17,
+          title: 'O Lobo da Estepe',
+          author: 'Hermann Hesse',
+          price: 49.90,
+          rating: 4.6,
+          reviewCount: 880,
+          image: '../images/o_lobo_da_estepe.jpg', // Imagem do livro "O Lobo da Estepe"
+        },
+
+        {
+          id: 18,
+          title: 'Crime e Castigo',
+          author: 'Fiódor Dostoyevsky',
+          price: 59.90,
+          rating: 4.8,
+          reviewCount: 1500,
+          image: '../images/crime_e_castigo.jpg', // Imagem do livro "Crime e Castigo"
+        },
+
+        {
+          id: 19,
+          title: 'A Insustentável Leveza do Ser',
+          author: 'Milan Kundera',
+          price: 45.90,
+          rating: 4.5,
+          reviewCount: 1100,
+          image: '../images/a_insustentavel_leveza_do_ser.jpg', // Imagem do livro "A Insustentável Leveza do Ser"
+        },
+
+        {
+          id: 20,
+          title: 'O Código da Vinci',
+          author: 'Dan Brown',
+          price: 49.90,
+          rating: 4.6,
+          reviewCount: 1500,
+          image: '../images/o_codigo_da_vinci.jpg', // Imagem do livro "O Código da Vinci"
+        },
           
         ],
         book: null, // Inicialmente null
